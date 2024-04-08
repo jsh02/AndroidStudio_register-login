490bd022b3d938b0df255f4a51e7e8bc2b5ea625
tmdghks <tmdghks@naver.com>
2024-04-08 18:08:50 +0900

    andorid-register 추가

--- app/src/main/java/com/example/sporting/MainActivity.java
+++ app/src/main/java/com/example/sporting/MainActivity.java
@@ -4,22 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth mFirebaseAuth;
+    DatabaseReference mDatabaseRef;
+
+    TextView tv_main;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Sporting-user");
+        tv_main = findViewById(R.id.tv_name_main);
+
+
 
         EdgeToEdge.enable(this);   // 위 코드는 네비게이션 바와 곂칠 수 있게 만들어주는 코드
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -27,6 +42,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        DatabaseReference userRef = mDatabaseRef.child("UserAccount").child(mFirebaseAuth.getCurrentUser().getUid());
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String name = dataSnapshot.child("name").getValue(String.class);
+                    // 닉네임을 maintitle TextView에 설정
+                    tv_main.setText(name + "님 환영합니다!");
+                }
+            }
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 에러 처리
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // 로그아웃 기능 실현
 
         Button btn_logout = findViewById(R.id.btn_logout_main);
         btn_logout.setOnClickListener(new View.OnClickListener() {
